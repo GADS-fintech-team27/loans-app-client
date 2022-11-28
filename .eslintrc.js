7e7aade8676d76d55f12857786ec9e068861e33f
aronmakiika <aronmakiika@gmail.com>
2022-11-28 20:42:13 +0300

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,19 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    'cypress/globals': true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
     'prettier',
+    'prettier/vue',
+    'plugin:nuxt/recommended',
+    'plugin:cypress/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: [],
+  plugins: ['cypress', 'prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': 'off',
+  },
 }
